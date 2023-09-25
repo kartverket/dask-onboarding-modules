0e8eaef7bf8bb0f785d3060e6b7148e19aeebfcf
Mari Røysheim <mari.roysheim@bekk.no>
2023-09-25 12:31:21 +0200

    Copy-paste modul for create workspace
    Dette er ren kopiering av kode fra dask-infrastructure.

---
+++ modules/dbx_workspace_create/main.tf
@@ -0,0 +1,48 @@
+module "create_vpc_for_databricks" {
+  source = "../gcp_vpc"
+
+  name_prefix  = "${var.name_prefix}-dbx"
+  name_postfix = var.name_postfix
+  project_id   = var.project_id
+  region       = var.region
+}
+
+resource "databricks_mws_networks" "this" {
+  provider     = databricks.accounts
+  account_id   = var.databricks_account_id
+  network_name = "${var.name_prefix}-vpc-${var.name_postfix}"
+  gcp_network_info {
+    network_project_id    = var.project_id
+    vpc_id                = module.create_vpc_for_databricks.vpc_name
+    subnet_id             = module.create_vpc_for_databricks.subnetwork_name
+    subnet_region         = var.region
+    pod_ip_range_name     = "pods"
+    service_ip_range_name = "svc"
+  }
+}
+
+resource "databricks_mws_workspaces" "this" {
+  provider       = databricks.accounts
+  account_id     = var.databricks_account_id
+  workspace_name = "${var.name_prefix}-dbxws-${var.name_postfix}"
+  location       = var.region
+
+  cloud_resource_container {
+    gcp {
+      project_id = var.project_id
+    }
+  }
+  network_id = databricks_mws_networks.this.network_id
+  gke_config {
+    connectivity_type = "PRIVATE_NODE_PUBLIC_MASTER"
+    master_ip_range   = "10.3.0.0/28"
+  }
+}
+
+output "workspace_url" {
+  value = databricks_mws_workspaces.this.workspace_url
+}
+
+output "workspace_id" {
+  value = databricks_mws_workspaces.this.workspace_id
+}
---
+++ modules/dbx_workspace_create/variables.tf
@@ -0,0 +1,20 @@
+variable "name_prefix" {
+  description = "String to be appended at the beginning of resource name properties."
+}
+
+variable "name_postfix" {
+  description = "String to be appended at the end of resource name properties."
+  default     = "tf-managed"
+}
+
+variable "project_id" {
+  description = "The Id of the project for the resources to create"
+}
+
+variable "region" {
+  description = "Location for Databricks workspace / region for GCP vpc. Databricks location should match vpc region."
+}
+
+variable "databricks_account_id" {
+  description = "Account ID found on https://accounts.gcp.databricks.com/"
+}
