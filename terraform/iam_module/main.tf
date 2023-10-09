64a16af435259cc7bcbd0c6c44e78c46c73cfecc
August Heiervang Dahl <augusthdahl@gmail.com>
2023-10-09 14:53:56 +0200

    Lag egen modul for IAM

---
+++ terraform/iam_module/main.tf
@@ -0,0 +1,38 @@
+locals {
+  landing_sa_roles = [
+    "roles/storage.objectViewer",
+    "roles/storage.legacyBucketReader",
+  ]
+}
+
+module "workspace_create" {
+  source = "./dbx_workspace_create/"
+
+  databricks_account_id = var.databricks_account_id
+  name_prefix           = var.name_prefix
+  project_id            = var.project_id
+  region                = var.region
+}
+
+resource "random_id" "bucket_suffix" {
+  byte_length = 4
+}
+
+resource "google_storage_bucket" "landing_zone" {
+  location = var.region
+  name     = "landing-zone-${var.env}-${var.team}-${random_id.bucket_suffix.hex}"
+}
+
+resource "google_service_account" "service_account" {
+  project      = var.project_id
+  account_id   = "cluster-${var.team}-sa"
+  display_name = "Service Account ${var.team}"
+  description  = "Service account som bare tilhører ${var.team}. I utgangspunktet har denne kun tilgang til der felles init-scripts blir lagret."
+}
+
+resource "google_storage_bucket_iam_member" "member" {
+  for_each = toset(local.landing_sa_roles)
+  bucket   = google_storage_bucket.landing_zone.name
+  role     = each.value
+  member   = "serviceAccount:${google_service_account.service_account.email}"
+}
---
+++ terraform/iam_module/variables.tf
@@ -0,0 +1,33 @@
+variable "name_prefix" {
+  description = "String to be appended at the beginning of resource name properties."
+}
+
+variable "name_postfix" {
+  description = "String to be appended at the end of resource name properties."
+  default     = "tf-managed"
+}
+
+variable "project_id" {
+  description = "The Id of the project for the resources to create"
+}
+
+variable "project_number" {
+  description = "The project number of the project for the resources to create"
+}
+
+variable "region" {
+  description = "Location for Databricks workspace / region for GCP vpc. Databricks location should match vpc region."
+  default     = "europe-west1"
+}
+
+variable "databricks_account_id" {
+  description = "Account ID found on https://accounts.gcp.databricks.com/"
+}
+
+variable "env" {
+  description = "Environment for the resources to create"
+}
+
+variable "team" {
+  description = "Team for the resources to create"
+}
