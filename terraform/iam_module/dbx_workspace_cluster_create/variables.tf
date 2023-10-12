ac8c6a8dc812f09ff85013d358e50dcac545f96a
August Heiervang Dahl <augusthdahl@gmail.com>
2023-10-12 08:08:04 +0200

    Flytt opprettelse av cluster fra dask-infrastructure

--- terraform/iam_module/dbx_workspace_cluster_create/variables.tf
+++ terraform/iam_module/dbx_workspace_cluster_create/variables.tf
@@ -1,23 +1,9 @@
-variable "name_postfix" {
-  description = "String to be appended at the end of resource name properties."
-  default     = "tf-managed"
-}
-
 variable "project_id" {
   description = "The Id of the project for the resources to create"
 }
 
-variable "region" {
-  description = "Location for Databricks workspace / region for GCP vpc. Databricks location should match vpc region."
-  default     = "europe-west1"
-}
-
-variable "databricks_account_id" {
-  description = "Account ID found on https://accounts.gcp.databricks.com/"
-}
-
 variable "env" {
-  description = "Environment for the resources to create"
+  description = "Environment"
 }
 
 variable "deploy_service_account" {
