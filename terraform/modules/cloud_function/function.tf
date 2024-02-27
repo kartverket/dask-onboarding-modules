a6cb53b4359f91a1a1f6ee08cadc2f3af943bac7
August Heiervang Dahl <augusthdahl@gmail.com>
2024-02-27 15:02:44 +0100

    Sett depends_on på cloud_run invoker

--- terraform/modules/cloud_function/function.tf
+++ terraform/modules/cloud_function/function.tf
@@ -53,11 +53,12 @@ resource "google_cloudfunctions2_function_iam_member" "invoker" {
 }
 
 resource "google_cloud_run_service_iam_member" "cloud_run_invoker" {
-  project  = google_cloudfunctions2_function.this.project
-  location = google_cloudfunctions2_function.this.location
-  service  = google_cloudfunctions2_function.this.name
-  role     = "roles/run.invoker"
-  member   = "serviceAccount:${var.service_account_email}"
+  project    = google_cloudfunctions2_function.this.project
+  location   = google_cloudfunctions2_function.this.location
+  service    = google_cloudfunctions2_function.this.name
+  role       = "roles/run.invoker"
+  member     = "serviceAccount:${var.service_account_email}"
+  depends_on = [google_cloudfunctions2_function.this]
 }
 
 resource "google_project_iam_member" "token_creator" {
