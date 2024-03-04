1f7dfe5a30f7c6df43976cea322b19a33a2449ca
August Heiervang Dahl <augusthdahl@gmail.com>
2024-03-04 13:38:13 +0100

    Fjern job-modul

--- terraform/modules/job_service_account/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "project_id" {
-  description = "The project ID to the product team's GCP project"
-  type        = string
-}
-
-variable "catalog_name" {
-  description = "The name of the product team's catalog"
-  type        = string
-}
