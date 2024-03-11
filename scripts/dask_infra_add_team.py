5a7f603e7e3e8d7f2ab2592e3091d4d5501da2f0
August Heiervang Dahl <augusthdahl@gmail.com>
2024-03-11 14:55:51 +0100

    Lag pull request i remote repo (#50)
    Setter opp en reusable workflow som gjør endringer i et repo og lager en
    PR basert på de endringene. Lager også et script med dummy-data for
    `dask-infrastructure` der vi kan legge inn ordentlig data senere

---
+++ scripts/dask_infra_add_team.py
@@ -0,0 +1,47 @@
+import sys
+import os
+
+
+def edit_file(filepath):
+    with open(filepath, 'r') as file:
+        lines = file.readlines()
+
+    # Define the new team data
+    new_team_data = """
+      "new_team" : {
+        "members" : [
+          "member1@example.com",
+          "member2@example.com"
+        ],
+        "external_users" : [],
+        "area_name" : "new_area",
+        "deploy_service_account" : {
+          "sandbox" : "new-team-deploy@sandbox.iam.gserviceaccount.com",
+          "dev" : "new-team-deploy@dev.iam.gserviceaccount.com"
+        },
+        "project_id" : {
+          "sandbox" : "new-team-sandbox",
+          "dev" : "new-team-dev"
+        }
+      },
+    """
+
+    # Find the end of the 'locals' block and insert the new team data
+    insert_index = next((i for i, line in enumerate(
+        reversed(lines)) if line.strip() == "},"), None)
+    if insert_index is not None:
+        lines.insert(len(lines) - insert_index, new_team_data)
+
+    with open(filepath, 'w') as file:
+        file.writelines(lines)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python edit_file.py <path-to-file>")
+        sys.exit(1)
+
+    file_path = sys.argv[1]
+    absolute_path = os.path.abspath(file_path)
+    print(f"Attempting to access: {absolute_path}")
+    edit_file(file_path)
