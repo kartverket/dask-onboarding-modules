04c48794679ac90f2e49525ba46b670a89d7c3ff
August Dahl <augusthdahl@gmail.com>
2024-04-04 15:29:59 +0200

    Endre pulls til pull

--- scripts/publish_to_firestore.py
+++ scripts/publish_to_firestore.py
@@ -6,7 +6,7 @@ def update_pull_request_url():
     step_id = os.environ["STEP_ID"]
     team = os.environ["TEAM_NAME"]
     api_url = os.environ["PULL_REQUEST_URL"]
-    url = api_url.replace("https://api.github.com/repos", "https://github.com")
+    url = api_url.replace("https://api.github.com/repos", "https://github.com").replace("pulls", "pull")
     db = firestore.Client()
     doc_ref = db.collection(u'onboarding').document(team)
     doc_ref.update({
