57f658824a67a44d931de25e48bbb2c5f8edeba9
August Dahl <augusthdahl@gmail.com>
2024-04-04 13:04:22 +0200

    Test list collection

--- scripts/publish_to_firestore.py
+++ scripts/publish_to_firestore.py
@@ -8,10 +8,18 @@ def update_pull_request_url():
     team = os.environ["TEAM_NAME"]
     url = os.environ["PULL_REQUEST_URL"]
     db = firestore.Client(project=project_id)
-    doc_ref = db.collection(u'onboarding').document(team)
-    doc_ref.update({
-        u'pr.`'+step_id+"`" : url,
-    })
+    docs = db.collection(u'onboarding').get()
+    print(docs)
+    # doc_ref = db.collection(u'onboarding').document(team)
+    # doc_ref.update({
+    #     u'pr.`'+step_id+"`" : url,
+    # })
 
 if __name__ == '__main__':
+    # os.environ["GCP_PROJECT_ID"] = "dataplattform-sandbox-6f27"
+    # os.environ["AUTH_PROJECT_ID"] = "698137771009"
+    # os.environ["SERVICE_ACCOUNT"] = "dataplattform-deploy@dataplattform-sandbox-6f27.iam.gserviceaccount.com"
+    # os.environ["TEAM_NAME"] = "spotify"
+    # os.environ["PULL_REQUEST_URL"] = "hahah.com"
+    # os.environ["STEP_ID"] = "dask-infrastructure-setup"
     update_pull_request_url()
