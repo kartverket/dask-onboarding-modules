11d6935eb53d2b96df8a574b382dad9d039bb906
August Dahl <augusthdahl@gmail.com>
2024-04-04 15:20:27 +0200

    Erstatt api-URL med faktisk PR-url

--- scripts/publish_to_firestore.py
+++ scripts/publish_to_firestore.py
@@ -5,7 +5,8 @@ from google.cloud import firestore
 def update_pull_request_url():
     step_id = os.environ["STEP_ID"]
     team = os.environ["TEAM_NAME"]
-    url = os.environ["PULL_REQUEST_URL"]
+    api_url = os.environ["PULL_REQUEST_URL"]
+    url = api_url.replace("https://api.github.com/repos", "https://github.com")
     db = firestore.Client()
     doc_ref = db.collection(u'onboarding').document(team)
     doc_ref.update({
@@ -13,10 +14,4 @@ def update_pull_request_url():
     })
 
 if __name__ == '__main__':
-    # os.environ["GCP_PROJECT_ID"] = "dataplattform-sandbox-6f27"
-    # os.environ["AUTH_PROJECT_ID"] = "698137771009"
-    # os.environ["SERVICE_ACCOUNT"] = "dataplattform-deploy@dataplattform-sandbox-6f27.iam.gserviceaccount.com"
-    # os.environ["TEAM_NAME"] = "spotify"
-    # os.environ["PULL_REQUEST_URL"] = "hahah.com"
-    # os.environ["STEP_ID"] = "dask-infrastructure-setup"
     update_pull_request_url()
