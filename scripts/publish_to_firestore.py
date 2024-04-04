e822c17275d8771581417cf84e2b08fd2226cf4f
August Dahl <augusthdahl@gmail.com>
2024-04-04 11:34:21 +0200

    Publiser URL til PR rett inn i teamets dokument i Firestore

---
+++ scripts/publish_to_firestore.py
@@ -0,0 +1,17 @@
+import os
+from google.cloud import firestore
+
+
+def update_pull_request_url():
+    project_id = os.environ["GCP_PROJECT_ID"]
+    step_id = os.environ["STEP_ID"]
+    team = os.environ["TEAM_NAME"]
+    url = os.environ["PULL_REQUEST_URL"]
+    db = firestore.Client(project=project_id)
+    doc_ref = db.collection(u'onboarding').document(team)
+    doc_ref.update({
+        u'pr.'+step_id : url,
+    })
+
+if __name__ == '__main__':
+    update_pull_request_url()
