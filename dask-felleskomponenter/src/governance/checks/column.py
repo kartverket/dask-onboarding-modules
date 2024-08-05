47d73071f2d5f755c16a68a6e5147b9c6eb69b5f
Jonas Mossin Wagle <jonas.mossin@gmail.com>
2024-08-05 17:46:35 +0200

    Flytt romlig representasjonstype til kolonnesjekk-fil

---
+++ dask-felleskomponenter/src/governance/checks/column.py
@@ -0,0 +1,18 @@
+from typing import List
+from .common import Errors, MetadataError
+from src.governance.main import TableMetadata
+
+def check_romlig_representasjonstype(metadata: TableMetadata, context: List) -> List[MetadataError]:
+    kodeliste_url = "https://register.geonorge.no/api/register/romlig-representasjonstype "
+
+    if metadata.romlig_representasjonstype is None:
+        error_obj = MetadataError(catalog=metadata.catalog, 
+                                     schema=metadata.schema, 
+                                     table=metadata.table, 
+                                     column=None, 
+                                     error_id=Errors.missing_romlig_representasjonstype, 
+                                     description="🔴 Feil: 'romlig_representasjonstype' mangler i column properties. Type: <romlig_representasjonstype> - gyldige verdier finner du her: " + kodeliste_url, 
+                                     solution=f"ALTER TABLE {metadata.catalog}.{metadata.schema}.{metadata.table} SET TBLPROPERTIES ( 'romlig_representasjonstype' = '<<SETT_ROMLIG_REPRESENTASJONSTYPE_HER>>')")
+        context.append(error_obj)
+    
+    return context
