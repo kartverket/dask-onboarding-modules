287e562f949cb37d56d503b292f49210fbbcd852
Jonas Mossin Wagle <jonas.mossin@gmail.com>
2024-08-05 17:04:41 +0200

    Endringer basert på tilbakemeldinger Thom

--- dask-felleskomponenter/src/governance/checks/common.py
+++ dask-felleskomponenter/src/governance/checks/common.py
@@ -3,9 +3,15 @@ from typing import Optional
 
 
 class Errors:
-    missing_owner = "missing-owner"
-    missing_origin = "missing-origin"
-    missing_description = "missing-description"
+    missing_beskrivelse = "missing-beskrivelse"
+    missing_tema = "missing-tema"
+    missing_emneord = "missing-emneord"
+    missing_begrep = "missing-begrep"
+    missing_tilgangsnivaa = "missing-tilgangsnivaa"
+    missing_medaljongnivaa = "missing-medaljongnivaa"
+    missing_epsg_koder = "missing-epsg_koder"
+    missing_romlig_representasjonstype = "missing-romlig_representasjonstype"
+    missing_bruksomraade = "missing-bruksomraade"
 
 @dataclass
 class MetadataError:
--- dask-felleskomponenter/src/governance/checks/table.py
+++ dask-felleskomponenter/src/governance/checks/table.py
@@ -2,190 +2,152 @@ from typing import List
 from src.governance.main import TableMetadata
 from .common import MetadataError, Errors
 
-def check_owner(metadata: TableMetadata, context: List) -> List:
-    if metadata.owner is None:
-        error_obj = MetadataError(catalog=metadata.catalog,
-                                     schema=metadata.schema,
-                                     table=metadata.table,
-                                     column=None,
-                                     error_id=Errors.missing_owner,
-                                     description="🔴 Error: Owner is missing from table properties",
-                                     solution=f"ALTER TABLE {metadata.catalog}.{metadata.schema}.{metadata.table} SET TBLPROPERTIES ( 'owner' = '<<SET_OWNER_HERE>>')")
-        context.append(error_obj)
-
-    return context
-
-def check_origin(metadata: TableMetadata, context: List) -> List:
-    if metadata.origin is None:
+def check_beskrivelse(metadata: TableMetadata, context: List) -> List[MetadataError]:
+    if metadata.beskrivelse is None:
         error_obj = MetadataError(catalog=metadata.catalog, 
                                      schema=metadata.schema, 
                                      table=metadata.table, 
                                      column=None, 
-                                     error_id=Errors.missing_description, 
-                                     description="🔴 Error: Origin is missing from table properties", 
-                                     solution=f"ALTER TABLE {metadata.catalog}.{metadata.schema}.{metadata.table} SET TBLPROPERTIES ( 'origin' = '<<SET_ORIGIN_HERE>>')")
-        context.append(error_obj)
-
-    return context
-
-def check_description(metadata: TableMetadata, context: List) -> List:
-    if metadata.description is None:
-        error_obj = MetadataError(catalog=metadata.catalog, 
-                                     schema=metadata.schema, 
-                                     table=metadata.table, 
-                                     column=None, 
-                                     error_id=Errors.missing_origin, 
-                                     description="🔴 Error: Description is missing from table properties", 
-                                     solution=f"ALTER TABLE {metadata.catalog}.{metadata.schema}.{metadata.table} SET TBLPROPERTIES ( 'description' = '<<SET_DESCRIPTION_HERE>>')")
+                                     error_id=Errors.missing_beskrivelse, 
+                                     description="🔴 Feil: 'beskrivelse' mangler i table properties", 
+                                     solution=f"ALTER TABLE {metadata.catalog}.{metadata.schema}.{metadata.table} SET TBLPROPERTIES ( 'beskrivelse' = '<<SETT_BESKRIVELSE_HER>>')")
         context.append(error_obj)
     
     return context
 
-def check_access_class(metadata: TableMetadata, context: List) -> List:
-    if metadata.access_class is None:
+def check_tilgangsnivaa(metadata: TableMetadata, context: List) -> List[MetadataError]:
+    if metadata.tilgangsnivaa is None:
         error_obj = MetadataError(catalog=metadata.catalog, 
                                      schema=metadata.schema, 
                                      table=metadata.table, 
                                      column=None, 
-                                     error_id=Errors.missing_origin, 
-                                     description="🔴 Error: Access class is missing from table properties", 
-                                     solution=f"ALTER TABLE {metadata.catalog}.{metadata.schema}.{metadata.table} SET TBLPROPERTIES ( 'access_class' = '<<SET_ACCESS_CLASS_HERE>>')")
+                                     error_id=Errors.missing_tilgangsnivaa, 
+                                     description="🔴 Feil: 'tilgangsnivaa' mangler i table properties", 
+                                     solution=f"ALTER TABLE {metadata.catalog}.{metadata.schema}.{metadata.table} SET TBLPROPERTIES ( 'tilgangsnivaa' = '<<SETT_TILGANGSNIVAA_HER>>')")
         context.append(error_obj)
     
     return context
 
-def check_valor(metadata: TableMetadata, context: List) -> List:
-    if metadata.valor is None:
+def check_medaljongnivaa(metadata: TableMetadata, context: List) -> List[MetadataError]:
+    if metadata.medaljongnivaa is None:
         error_obj = MetadataError(catalog=metadata.catalog, 
                                      schema=metadata.schema, 
                                      table=metadata.table, 
                                      column=None, 
-                                     error_id=Errors.missing_origin, 
-                                     description="🔴 Error: Valor is missing from table properties", 
-                                     solution=f"ALTER TABLE {metadata.catalog}.{metadata.schema}.{metadata.table} SET TBLPROPERTIES ( 'valor' = '<<SET_VALOR_HERE>>')")
+                                     error_id=Errors.missing_medaljongnivaa, 
+                                     description="🔴 Feil: 'medaljongnivaa' mangler i table properties", 
+                                     solution=f"ALTER TABLE {metadata.catalog}.{metadata.schema}.{metadata.table} SET TBLPROPERTIES ( 'medaljongnivaa' = '<<SETT_MEDALJONGNIVAA_HER>>')")
         context.append(error_obj)
     
     return context
 
-def check_title(metadata: TableMetadata, context: List) -> List:
-    if metadata.title is None:
-        error_obj = MetadataError(catalog=metadata.catalog, 
-                                     schema=metadata.schema, 
-                                     table=metadata.table, 
-                                     column=None, 
-                                     error_id=Errors.missing_origin, 
-                                     description="🔴 Error: Title is missing from table properties", 
-                                     solution=f"ALTER TABLE {metadata.catalog}.{metadata.schema}.{metadata.table} SET TBLPROPERTIES ( 'title' = '<<SET_TITLE_HERE>>')")
-        context.append(error_obj)
-    
-    return context
-
-def check_privacy_considerations(metadata: TableMetadata, context: List) -> List:
+def check_bruksvilkaar(metadata: TableMetadata, context: List) -> List[MetadataError]:
     if metadata.privacy_considerations is None:
         error_obj = MetadataError(catalog=metadata.catalog, 
                                      schema=metadata.schema, 
                                      table=metadata.table, 
                                      column=None, 
                                      error_id=Errors.missing_origin, 
-                                     description="🔴 Error: Privacy considerations is missing from table properties", 
-                                     solution=f"ALTER TABLE {metadata.catalog}.{metadata.schema}.{metadata.table} SET TBLPROPERTIES ( 'privacy_considerations' = '<<SET_PRIVACY_CONSIDERATIONS_HERE>>')")
+                                     description="🔴 Feil: 'bruksvilkaar' mangler i table properties", 
+                                     solution=f"ALTER TABLE {metadata.catalog}.{metadata.schema}.{metadata.table} SET TBLPROPERTIES ( 'privacy_considerations' = '<<SETT_PRIVACY_CONSIDERATIONS_HER>>')")
         context.append(error_obj)
     
     return context
 
-def check_theme(metadata: TableMetadata, context: List) -> List:
-    if metadata.theme is None:
+def check_tema(metadata: TableMetadata, context: List) -> List[MetadataError]:
+    if metadata.tema is None:
         error_obj = MetadataError(catalog=metadata.catalog, 
                                      schema=metadata.schema, 
                                      table=metadata.table, 
                                      column=None, 
-                                     error_id=Errors.missing_origin, 
-                                     description="🔴 Error: Theme is missing from table properties", 
-                                     solution=f"ALTER TABLE {metadata.catalog}.{metadata.schema}.{metadata.table} SET TBLPROPERTIES ( 'theme' = '<<SET_THEME_HERE>>')")
+                                     error_id=Errors.missing_tema, 
+                                     description="🔴 Feil: 'tema' mangler i table properties", 
+                                     solution=f"ALTER TABLE {metadata.catalog}.{metadata.schema}.{metadata.table} SET TBLPROPERTIES ( 'tema' = '<<SETT_TEMA_HER>>')")
         context.append(error_obj)
     
     return context
 
-def check_keyword(metadata: TableMetadata, context: List) -> List:
-    if metadata.keyword is None:
+def check_emneord(metadata: TableMetadata, context: List) -> List[MetadataError]:
+    if metadata.emneord is None:
         error_obj = MetadataError(catalog=metadata.catalog, 
                                      schema=metadata.schema, 
                                      table=metadata.table, 
                                      column=None, 
-                                     error_id=Errors.missing_origin, 
-                                     description="🔴 Error: Keyword is missing from table properties", 
-                                     solution=f"ALTER TABLE {metadata.catalog}.{metadata.schema}.{metadata.table} SET TBLPROPERTIES ( 'keyword' = '<<SET_KEYWORD_HERE>>')")
+                                     error_id=Errors.missing_emneord, 
+                                     description="🔴 Feil: 'emneord' mangler i table properties", 
+                                     solution=f"ALTER TABLE {metadata.catalog}.{metadata.schema}.{metadata.table} SET TBLPROPERTIES ( 'emneord' = '<<SETT_emneord_HER>>')")
         context.append(error_obj)
     
     return context
 
 
 # Spør thom om denne
-def check_epsg_codes(metadata: TableMetadata, context: List) -> List:
-    if metadata.epsg_codes is None:
+def check_epsg_koder(metadata: TableMetadata, context: List) -> List[MetadataError]:
+    if metadata.epsg_koder is None:
         error_obj = MetadataError(catalog=metadata.catalog, 
                                      schema=metadata.schema, 
                                      table=metadata.table, 
                                      column=None, 
-                                     error_id=Errors.missing_origin, 
-                                     description="🔴 Error: Epsg codes is missing from table properties", 
-                                     solution=f"ALTER TABLE {metadata.catalog}.{metadata.schema}.{metadata.table} SET TBLPROPERTIES ( 'epsg_codes' = '<<SET_EPSG_CODES_HERE>>')")
+                                     error_id=Errors.missing_epsg_koder, 
+                                     description="🔴 Feil: 'epsg_koder' mangler i table properties", 
+                                     solution=f"ALTER TABLE {metadata.catalog}.{metadata.schema}.{metadata.table} SET TBLPROPERTIES ( 'epsg_koder' = '<<SETT_EPSG_KODER_HER>>')")
         context.append(error_obj)
     
     return context
 
-def check_spatial_representation(metadata: TableMetadata, context: List) -> List:
-    if metadata.spatial_representation is None:
+def check_romlig_representasjonstype(metadata: TableMetadata, context: List) -> List[MetadataError]:
+    if metadata.romlig_representasjonstype is None:
         error_obj = MetadataError(catalog=metadata.catalog, 
                                      schema=metadata.schema, 
                                      table=metadata.table, 
                                      column=None, 
-                                     error_id=Errors.missing_origin, 
-                                     description="🔴 Error: Spatial representation is missing from table properties", 
-                                     solution=f"ALTER TABLE {metadata.catalog}.{metadata.schema}.{metadata.table} SET TBLPROPERTIES ( 'spatial_representation' = '<<SET_SPATIAL_REPRESENTATION_HERE>>')")
+                                     error_id=Errors.romlig_representasjonstype, 
+                                     description="🔴 Feil: 'romlig_representasjonstype' mangler i table properties", 
+                                     solution=f"ALTER TABLE {metadata.catalog}.{metadata.schema}.{metadata.table} SET TBLPROPERTIES ( 'romlig_representasjonstype' = '<<SETT_ROMLIG_REPRESENTASJONSTYPE_HER>>')")
         context.append(error_obj)
     
     return context
 
-def check_usage(metadata: TableMetadata, context: List) -> List:
-    if metadata.usage is None:
+def check_bruksomraade(metadata: TableMetadata, context: List) -> List[MetadataError]:
+    if metadata.bruksomraade is None:
         error_obj = MetadataError(catalog=metadata.catalog, 
                                      schema=metadata.schema, 
                                      table=metadata.table, 
                                      column=None, 
-                                     error_id=Errors.missing_origin, 
-                                     description="🔴 Error: Usage is missing from table properties", 
-                                     solution=f"ALTER TABLE {metadata.catalog}.{metadata.schema}.{metadata.table} SET TBLPROPERTIES ( 'usage' = '<<SET_USAGE_HERE>>')")
+                                     error_id=Errors.bruksomraade, 
+                                     description="🔴 Feil: bruksomraade mangler i table properties", 
+                                     solution=f"ALTER TABLE {metadata.catalog}.{metadata.schema}.{metadata.table} SET TBLPROPERTIES ( 'bruksomraade' = '<<SETT_BRUKSOMRAADE_HER>>')")
         context.append(error_obj)
     
     return context
 
-def check_concept(metadata: TableMetadata, context: List) -> List:
-    if metadata.concept is None:
+def check_begrep(metadata: TableMetadata, context: List) -> List[MetadataError]:
+    if metadata.begrep is None:
         error_obj = MetadataError(catalog=metadata.catalog, 
                                      schema=metadata.schema, 
                                      table=metadata.table, 
                                      column=None, 
-                                     error_id=Errors.missing_origin, 
-                                     description="🔴 Error: Concept is missing from table properties", 
-                                     solution=f"ALTER TABLE {metadata.catalog}.{metadata.schema}.{metadata.table} SET TBLPROPERTIES ( 'concept' = '<<SET_CONCEPT_HERE>>')")
+                                     error_id=Errors.missing_begrep, 
+                                     description="🔴 Feil: 'begrep' mangler i table properties", 
+                                     solution=f"ALTER TABLE {metadata.catalog}.{metadata.schema}.{metadata.table} SET TBLPROPERTIES ( 'begrep' = '<<SETT_BEGREP_HER>>')")
         context.append(error_obj)
     
     return context
     
 checks_for_valor = {
-    "bronze": [check_title, check_description, check_access_class, check_privacy_considerations],
-    "silver": [check_title, check_description, check_theme, check_keyword, check_access_class, check_epsg_codes, check_spatial_representation, check_usage, check_privacy_considerations],
-    "gold":   [check_title, check_description, check_theme, check_keyword, check_concept, check_access_class, check_epsg_codes, check_spatial_representation, check_usage, check_privacy_considerations],
+    "bronze": [check_beskrivelse, check_tilgangsnivaa],
+    "silver": [check_beskrivelse, check_tema, check_emneord, check_tilgangsnivaa, check_epsg_koder, check_romlig_representasjonstype, check_bruksomraade],
+    "gold":   [check_beskrivelse, check_tema, check_emneord, check_begrep, check_tilgangsnivaa, check_epsg_koder, check_romlig_representasjonstype, check_bruksomraade],
 }
 
-def validate_table(metadata: TableMetadata) -> List[str]:
-    validation_context = check_valor(metadata, [])
+
+def validate_table(metadata: TableMetadata) -> List[MetadataError]:
+    validation_context = check_medaljongnivaa(metadata, [])
 
     if len(validation_context) > 0:
         return validation_context
     
-    for check in checks_for_valor[metadata.valor]:
+    for check in checks_for_valor[metadata.medaljongnivaa]:
         validation_context = check(metadata, validation_context)
 
     return validation_context
