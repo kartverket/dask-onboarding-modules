11333a46d500d66fa1d2ccf10eaf123b78ea61f8
Jonas Mossin Wagle <jonas.mossin@gmail.com>
2024-08-06 15:40:51 +0200

    Vær eksplisitt i uthenting av metadata

--- dask-felleskomponenter/src/governance/main.py
+++ dask-felleskomponenter/src/governance/main.py
@@ -12,13 +12,22 @@ class Metadata:
         self.spark = spark
     
     def get_table_metadata(self) -> TableMetadata:
-        df = self.spark.sql(f"SHOW TBLPROPERTIES {self.catalog}.{self.schema}.{self.table}")
+        df_tbltags = self.spark.sql(f"SELECT catalog_name, schema_name, table_name, tag_name, tag_value FROM information_schema.table_tags WHERE catalog_name = '{self.catalog}' AND schema_name = '{self.schema}' AND table_name = '{self.table}';")
+        
         keys = { }
-        for r in df.collect():
+
+        for r in df_tbltags.collect():
             if "delta." in r["key"]: # Ignorer delta spesifikke properties
                 continue
-            keys[r["key"]] = r["value"]
-        return TableMetadata(**keys)
+            keys[r["tag_name"]] = r["tag_value"]
+        
+        return TableMetadata(
+            catalog=self.catalog, schema=self.schema, table=self.table, 
+            beskrivelse=keys["beskrivelse"], tilgangsnivaa=keys["tilgangsnivaa"], 
+            medaljongnivaa=keys["medaljongnivaa"], tema=keys["tema"], 
+            emneord=keys["emneord"], epsg_koder=keys["epsg_koder"], 
+            bruksomraade=keys["bruksomraade"], begrep=keys["begrep"]
+        )
     
     def get_table_column_metadata(self) -> Any: # Lage denne typen i annen oppgave
         df = self.spark.sql(f"""
