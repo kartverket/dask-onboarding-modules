1d55f3203bb69eb5860cf395085b77e6c94cc19c
August Dahl <augusthdahl@gmail.com>
2024-08-08 14:36:20 +0200

    Fjern EPSG-sjekk på tabell-nivå

--- dask-felleskomponenter/setup.py
+++ dask-felleskomponenter/setup.py
@@ -7,7 +7,7 @@ packages = setuptools.find_packages(where="src")
 
 setuptools.setup(
     name="dask-felleskomponenter",
-    version="0.0.19",
+    version="0.0.20",
     author="Dataplattform@Statens Kartverk",
     author_email="dataplattform@kartverket.no",
     description="Felleskomponenter på DASK",
--- dask-felleskomponenter/src/dask_felleskomponenter/governance/checks/table.py
+++ dask-felleskomponenter/src/dask_felleskomponenter/governance/checks/table.py
@@ -49,17 +49,6 @@ def check_emneord(metadata: TableMetadata, context: List[MetadataError]) -> List
     
     return context
 
-
-# Spør thom om denne
-def check_epsg_koder(metadata: TableMetadata, context: List[MetadataError]) -> List[MetadataError]:
-    kodeliste_url = "https://register.geonorge.no/api/register/epsg-koder"
-
-    if not check_codelist_value(kodeliste_url, metadata.epsg_koder, override_kodeliste_keyword="epsgcode"):
-        valid_values = get_valid_codelist_values(kodeliste_url, "epsgcode")
-        context.append(_generate_metadata_error(metadata.catalog, metadata.schema, metadata.table, "epsg_koder", "epsg_koder", metadata.epsg_koder == None, f"gyldige verdier: {valid_values}"))
-    
-    return context
-
 def check_bruksomraade(metadata: TableMetadata, context: List[MetadataError]) -> List[MetadataError]:
     kodeliste_url = "https://register.geonorge.no/metadata-kodelister/formal"
 
