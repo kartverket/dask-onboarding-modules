afe179917a453b2ffbfed8131a7677a2bdc8c284
August Dahl <augusthdahl@gmail.com>
2024-08-08 13:52:48 +0200

    bump

--- dask-felleskomponenter/setup.py
+++ dask-felleskomponenter/setup.py
@@ -7,7 +7,7 @@ packages = setuptools.find_packages(where="src")
 
 setuptools.setup(
     name="dask-felleskomponenter",
-    version="0.0.17",
+    version="0.0.18",
     author="Dataplattform@Statens Kartverk",
     author_email="dataplattform@kartverket.no",
     description="Felleskomponenter på DASK",
