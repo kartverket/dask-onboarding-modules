d20647670226ca789ca18b06afbe467d29e8e7e3
August Dahl <augusthdahl@gmail.com>
2024-08-08 11:31:23 +0200

    Fjern spark-input i Metadata-objektet

--- dask-felleskomponenter/setup.py
+++ dask-felleskomponenter/setup.py
@@ -7,7 +7,7 @@ packages = setuptools.find_packages(where="src")
 
 setuptools.setup(
     name="dask-felleskomponenter",
-    version="0.0.13",
+    version="0.0.16",
     author="Dataplattform@Statens Kartverk",
     author_email="dataplattform@kartverket.no",
     description="Felleskomponenter på DASK",
--- dask-felleskomponenter/src/dask_felleskomponenter/tests/test_table_validation.py
+++ dask-felleskomponenter/src/dask_felleskomponenter/tests/test_table_validation.py
@@ -17,7 +17,7 @@ class TestValidateTable(unittest.TestCase):
         # Arrange
         gold_mock_json_data = read_file("example_table_metadata_gold.json")
         Metadata.get_table_metadata = MagicMock(return_value=TableMetadata(**gold_mock_json_data))
-        metadata = Metadata("catalog", "schema", "table", None)
+        metadata = Metadata("catalog", "schema", "table")
 
         # Act
         metadata.validate()
@@ -29,7 +29,7 @@ class TestValidateTable(unittest.TestCase):
         # Arrange
         gold_mock_json_data = read_file("example_table_metadata_gold.json")
         Metadata.get_table_metadata = MagicMock(return_value=TableMetadata(**gold_mock_json_data))
-        metadata = Metadata("catalog", "schema", "table", None)
+        metadata = Metadata("catalog", "schema", "table")
 
         # Act
         result = metadata.validate()
