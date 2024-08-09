b8b99594703b585d69b6e2cc09f454ed360a0881
August Heiervang Dahl <augusthdahl@gmail.com>
2024-08-09 11:57:16 +0200

    Fiks PyPI-workflow (#95)
    Var nødt til å gjøre noen endringer for å få det til å funke som det
    skulle. Blir publisert til PyPI nå ved manuell trigging i Actions
    
    ---------
    
    Co-authored-by: GitHub Actions <actions@github.com>

--- dask-felleskomponenter/setup.py
+++ dask-felleskomponenter/setup.py
@@ -7,7 +7,7 @@ packages = setuptools.find_packages(where="src")
 
 setuptools.setup(
     name="dask-felleskomponenter",
-    version="0.0.21",
+    version="0.0.22",
     author="Dataplattform@Statens Kartverk",
     author_email="dataplattform@kartverket.no",
     description="Felleskomponenter på DASK",
