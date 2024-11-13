e72f9182628dfea855c611abb59bc3727b24fdcd
Tom Ellett von Brasch <elltho@kartverket.no>
2024-11-13 14:53:51 +0100

    Update table.py
    Bytt ut 'tema' med hovedkategori, oppdater kodelisten som brukes

--- dask-felleskomponenter/src/dask_felleskomponenter/governance/checks/table.py
+++ dask-felleskomponenter/src/dask_felleskomponenter/governance/checks/table.py
@@ -34,12 +34,12 @@ def check_medaljongnivaa(metadata: TableMetadata, context: List[MetadataError]) 
     
     return context
 
-def check_tema(metadata: TableMetadata, context: List[MetadataError]) -> List[MetadataError]:
-    kodeliste_url = "https://register.geonorge.no/api/register/inspiretema"
+def check_hovedkategori(metadata: TableMetadata, context: List[MetadataError]) -> List[MetadataError]:
+    kodeliste_url = "https://register.geonorge.no/metadata-kodelister/tematisk-hovedkategori"
 
-    if not check_codelist_value(kodeliste_url, metadata.tema):
+    if not check_codelist_value(kodeliste_url, metadata.hovedkategori):
         valid_values = get_valid_codelist_values(kodeliste_url)
-        context.append(_generate_metadata_error(metadata.catalog, metadata.schema, metadata.table, "tema", "inspiretema", metadata.tema == None, f"gyldige verdier: {valid_values}", valid_values=valid_values))
+        context.append(_generate_metadata_error(metadata.catalog, metadata.schema, metadata.table, "hovedkategori", "hovedkategori", metadata.tema == None, f"gyldige verdier: {valid_values}", valid_values=valid_values))
     
     return context
 
