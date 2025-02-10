2dc58ac5619a387897984b2dbb7c09c0f1bf14c5
Stian Mogen <stfjae@gmail.com>
2025-02-10 13:44:00 +0100

    iterate over skyporten buckets

--- terraform/modules/skyporten_workload_identity/outputs.tf
+++ terraform/modules/skyporten_workload_identity/outputs.tf
@@ -1,3 +1,3 @@
-output "gcs_bucket_name" {
-  value = module.skyporten_consumer.gcs_bucket_name
+output "gcs_bucket_names" {
+  value = { for key, mod in module.skyporten_consumer : key => mod.gcs_bucket_name }
 }
